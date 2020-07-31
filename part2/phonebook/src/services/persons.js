250dc6dee27e59509083c4ca4df71781bbea04d3
Sergey Frolov <sergey.frolov@sunlightphotonics.com>
2020-07-31 19:33:53 -0400

    Production build for step11

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const url = 'http://localhost:3001/api/persons'
+const url = 'https://sleepy-reef-85544.herokuapp.com/api/persons'
 
 const getAll = () => axios.get(url).then(response => response.data)
 const create = person => axios.post(url, person).then(response => response.data)
