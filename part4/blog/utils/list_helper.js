bd54afd840c2e573a3733ceae77966feca969257
Sergey Frolov <sergey.frolov@sunlightphotonics.com>
2020-08-04 20:45:02 -0400

    unit tests step3 done

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -6,4 +6,8 @@ const totalLikes = blogs => {
   return blogs.reduce((s,v) => s+v.likes, 0)
 }
 
-module.exports = { dummy, totalLikes }
+const favoriteBlog = blogs => {
+  return blogs.reduce((s,v) => (!s || v.likes>s.likes) ? v : s, null)
+}
+
+module.exports = { dummy, totalLikes, favoriteBlog }
