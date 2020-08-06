ba40c0b41981e71539c5c57fa3294cd28140da4e
Sergey Frolov <sergey.frolov@sunlightphotonics.com>
2020-08-05 21:48:38 -0400

    added minor middleware

--- part4/blog/app.js
+++ part4/blog/app.js
@@ -20,5 +20,7 @@ app.use(cors())
 app.use(express.json())
 app.use(middleware.requestLogger)
 app.use('/api/blogs/', blogRouter)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -2,7 +2,6 @@ const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
 
 blogRouter.get('/', (request, response) => {
-  console.log(request)
   Blog
     .find({})
     .then(blogs => {
