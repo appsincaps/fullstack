acc446bf3f4182ed790a9708d06e10628e1b34b2
Sergey Frolov <sergey.frolov@sunlightphotonics.com>
2020-08-10 14:58:19 -0400

    working with three api tests

--- part4/blog/tests/blog_api.test.js
+++ part4/blog/tests/blog_api.test.js
@@ -10,6 +10,18 @@ test('api test: blogs are returned as json', async () => {
     .expect('Content-Type', /application\/json/)
 })
 
+test('api test: there are three blogs', async () => {
+  const response = await api.get('/api/blogs')
+
+  expect(response.body).toHaveLength(3)
+})
+
+test('api test: the 2nd blog is about Sunlight', async () => {
+  const response = await api.get('/api/blogs')
+
+  expect(response.body[1].title).toBe('Sunlight')
+})
+
 afterAll(() => {
   mongoose.connection.close()
 })
