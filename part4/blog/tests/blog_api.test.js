da19dd7292b4c1d208a002838a64d0bd87215cd8
Sergey Frolov <sergey.frolov@sunlightphotonics.com>
2020-08-10 20:54:39 -0400

    blog api tests step1 done

--- part4/blog/tests/blog_api.test.js
+++ part4/blog/tests/blog_api.test.js
@@ -2,6 +2,19 @@ const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 const api = supertest(app)
+const helper = require('./test_helper')
+const Blog = require('../models/blog')
+const blog = require('../models/blog')
+
+beforeEach(async () => {
+
+  await Blog.deleteMany({})
+
+  const blogs = helper.initialBlogs.map(blog => new Blog(blog))
+  const promises = blogs.map(blog => blog.save())
+
+  await Promise.all(promises)
+})
 
 test('api test: blogs are returned as json', async () => {
   await api
@@ -10,16 +23,56 @@ test('api test: blogs are returned as json', async () => {
     .expect('Content-Type', /application\/json/)
 })
 
-test('api test: there are three blogs', async () => {
+test('api test: there are two blogs in db', async () => {
   const response = await api.get('/api/blogs')
-
-  expect(response.body).toHaveLength(3)
+  expect(response.body).toHaveLength(helper.initialBlogs.length)
 })
 
 test('api test: the 2nd blog is about Sunlight', async () => {
   const response = await api.get('/api/blogs')
+  const contents = response.body.map(r => r.title)
+  expect(contents).toContain(
+    'Sunlight'
+  )
+})
 
-  expect(response.body[1].title).toBe('Sunlight')
+test('api test: adding a new blog', async () => {
+
+  const blog = new Blog({
+    title: 'Test blog',
+    author: 'AI',
+    url: 'www.everywhere.net',
+    likes: 100
+  })
+
+  await api
+    .post('/api/blogs')
+    .send(blog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const blogs = await helper.blogsInDb()
+  const titles = blogs.map(r => r.title)
+
+  expect(blogs).toHaveLength(helper.initialBlogs.length + 1)
+  expect(titles).toContain('Test blog')
+})
+
+test('api test: a blog without title will not be added', async () => {
+
+  const blog = new Blog({
+    author: 'AI',
+    url: 'www.everywhere.net',
+    likes: 100
+  })
+
+  await api
+    .post('/api/blogs')
+    .send(blog)
+    .expect(400)
+
+  const blogs = await helper.blogsInDb()
+  expect(blogs).toHaveLength(helper.initialBlogs.length)
 })
 
 afterAll(() => {
---
+++ part4/blog/tests/test_helper.js
@@ -0,0 +1,38 @@
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+  {
+    title: "Google me",
+    author: "SF",
+    url: "www.google.com",
+    likes: 0
+  },
+  {
+    title: "Sunlight",
+    author: "SF",
+    url: "www.sunlightx.com",
+    likes: 1
+  }
+]
+
+const nonExistingId = async () => {
+  const blog = new Blog({
+    title: "None",
+    author: "",
+    url: "xxx",
+    likes: 0
+  },)
+  await blog.save()
+  await blog.remove()
+
+  return blog._id.toString()
+}
+
+const blogsInDb = async () => {
+  const blogs = await Blog.find({})
+  return blogs.map(blog => blog.toJSON())
+}
+
+module.exports = {
+  initialBlogs, nonExistingId, blogsInDb
+}
