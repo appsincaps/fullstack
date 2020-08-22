0a7073727f4d9cd387c1a40fea9f6d64cfe8ed3b
Sergey Frolov <sergey.frolov@sunlightphotonics.com>
2020-08-21 22:39:49 -0400

    bloglist frontend step7 done

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,9 +1,31 @@
-import React from 'react'
-const Blog = ({ blog, deleteBlog }) => (
-  <div>
-    {blog.title} by {blog.author} 
-    <button onClick={() => deleteBlog(blog)}> delete </button>
-  </div>
-)
+import React, { useState } from 'react'
+const Blog = ({ blog, deleteBlog }) => {
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const [ expand, setExpand ] = useState(false)
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} by {blog.author} 
+      <button onClick={() => deleteBlog(blog)}> delete </button>
+      <button onClick={() => setExpand(!expand)}>{expand ? 'hide' : 'view'}</button>
+      {expand &&
+        <div>
+          <div>{blog.url}</div>
+          <div>likes {blog.likes} <button>Like</button></div>
+          <div>{blog.user.name}</div>
+        </div>
+      }
+    </div>
+  )
+
+}
 
 export default Blog
